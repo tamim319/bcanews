961c95e6c40b2aae74991cdd459ad7cdc75a4fd8
Tamim-uddin <muhammadtamimuddin@gmail.com>
2022-06-18 00:56:27 +0600

    set

---
+++ src/Pages/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgRk_fxlRs8HW9Zr_CeJcLz6fCOURPuRQ",
+    authDomain: "bca-news.firebaseapp.com",
+    projectId: "bca-news",
+    storageBucket: "bca-news.appspot.com",
+    messagingSenderId: "758927002266",
+    appId: "1:758927002266:web:4964442fda438e15e7ed23",
+    measurementId: "G-QSK2CZJXE8"
+  };
+
+  export default firebaseConfig;
---
+++ src/Pages/Home/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const home = () => {
+    return (
+        <div>
+            <h2>TAmim</h2>
+        </div>
+    );
+};
+
+export default home;
